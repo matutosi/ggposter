bcc66c47f6ef83be27f2e18f1ce0c597cf55bd60
matutosi <matutosi@gmail.com>
2022-02-25 11:29:48 +0900

    add package document

---
+++ R/ggposter.R
@@ -0,0 +1,11 @@
+#' ggposter: Easy publication for poster with ggplot2
+#' 
+#' The ggposter package provide tools for easy publication of poster 
+#' with ggplot2. 
+#' 
+#' # @section 
+#' 
+#' @docType package
+#' @name ggposter
+NULL
+#> NULL
