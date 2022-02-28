09465b25737f81c8b04594d5fe989df225aaf2a7
matutosi <matutosi@gmail.com>
2022-02-28 10:13:21 +0900

    add argument: silent in get_font_size

--- R/textgrob.R
+++ R/textgrob.R
@@ -1,12 +1,7 @@
   #' Wrapper function of converting text into arranged text grob.
   #' 
-  #' @param ... Text object(s). Multiple object can be input. 
-  #'        Object name of text: foo_useword or foo_keyword_bar
-  #'        examples: txt_title, txt_body_1, proj_ITEM_big
-  #'        useword: "tiny", "script", "footnote", "caption", 
-  #'                 "body", "BODY", "item", "ITEM", 
-  #'                 "thank", "author", "affil", 
-  #'                 "subt", "kw", or "title"
+  #' @inheritParams txt2tibble
+  #' @param silent Logical. TRUE: no massage, FALSE: shows massage. 
   #' @return textGrob
   #' @examples
   #' txt_title    <- "This is a title."
@@ -18,7 +13,7 @@
   #' grid::grid.draw(arranged_txt)
   #' 
   #' @export
-arrange_txt <- function(...){
+arrange_txt <- function(..., silent=TRUE){
   tg <- 
     txt2tibble(...) %>%
     purrr::pmap(as_tg)
@@ -30,23 +25,9 @@ arrange_txt <- function(...){
   #' 
   #' @param text      A string. 
   #' @param font_size A numeric of font size.
-  #' @param base A numeric of base font size: 
-  #'             8, 10, 12, 14, 18, 22, 26, 32, or 38)
-  #'             If base=NULL, base=26 will be set in get_font_size(). 
-  #' @param name A string of font size name. 
-  #'             "tiny", "scriptsize", "footnotesize", "small", 
-  #'             "normalsize", "large", "Large", "LARGE", 
-  #'             "huge", "Huge", or "HUGE"
-  #' @param use A string of font size name for use. 
-  #'             "tiny", "script", "footnote", "caption", 
-  #'             "body", "BODY", "item", "ITEM", "thank",
-  #'             "author", "affil", "subt", "kw", or "title"
-  #'               thank: acknowledgement
-  #'               affil: affiliation
-  #'               subt: subtitle
-  #'               kw: keyword
+  #' @inheritParams get_font_size
   #' @export
-as_tg <- function(text, font_size=NULL, base=NULL, name=NULL, use=NULL){
+as_tg <- function(text, font_size=NULL, base=NULL, name=NULL, use=NULL, silent=TRUE){
     # check input arguments
   if(!check_text(text))         return(NULL)
   if(!check_text(use))          return(NULL)
@@ -55,7 +36,7 @@ as_tg <- function(text, font_size=NULL, base=NULL, name=NULL, use=NULL){
   if(!check_numeric(base))      return(NULL)
     # font size
   if(is.null(font_size)){
-    font_size <- get_font_size(base, name, use)
+    font_size <- get_font_size(base, name, use, silent)
   }
     # make grob
   grid::textGrob(label=text, gp=grid::gpar(fontsize=font_size), name=use)
@@ -79,8 +60,8 @@ as_tg <- function(text, font_size=NULL, base=NULL, name=NULL, use=NULL){
   #' txt_ITEM_1 <- "ITEM text is larger than item text."
   #' txt_item_2 <- "This is a item text, too."
   #' txt_BODY_1 <- "BODY text is larger than body text."
-  #' txt_body_1 <- "This is a body text, too."
-  #' txt2tibble(txt_title, txt_ITEM_1, txt_item_2, txt_BODY_1, txt_body_1)
+  #' txt_body_2 <- "This is a body text, too."
+  #' txt2tibble(txt_title, txt_ITEM_1, txt_item_2, txt_BODY_1, txt_body_2)
   #' 
   #' @export
 txt2tibble <- function(...){
@@ -110,6 +91,7 @@ txt2tibble <- function(...){
   #'               affil: affiliation
   #'               subt: subtitle
   #'               kw: keyword
+  #' @param silent Logical. TRUE: no massage, FALSE: shows massage. 
   #' @return A numeric of font size.
   #' @seealso font_size_list
   #' @examples
@@ -119,17 +101,17 @@ txt2tibble <- function(...){
   #' get_font_size(base=22,   name=NULL,    use="kw")
   #' 
   #' @export
-get_font_size <- function(base=NULL, name=NULL, use=NULL){
+get_font_size <- function(base=NULL, name=NULL, use=NULL, silent=TRUE){
   if(is.null(base)){
-    message("26pt is used for base size.")
+    if(!silent) message("26pt is used for base size.")
     base <- 26
   }
   if(is.null(name) & is.null(use)){
-    message("input must be ONE of name or use. 26pt is used.")
+    if(!silent) message("input must be ONE of name or use. 26pt is used.")
     return(26)
   }
   if(!is.null(name) & !is.null(use)){
-    message("'use' is used to set font size. Please input one of name OR use.") 
+    if(!silent) message("'use' is used to set font size. Please input one of name OR use.") 
     name <- NULL
   }
   utils::data("font_size_list")
@@ -139,32 +121,28 @@ get_font_size <- function(base=NULL, name=NULL, use=NULL){
   font$size
 }
 
-  #' Layout  
+  #' Create layout to fit height and width with text grob. 
   #' 
   #' @param tg A text grob
-  #' @param width A numeric. 
-  #' @param height A numeric. 
-  #' @param row_margin A numeric. 
+  #' @param width A numeric. Default is null to fit text grob. 
+  #' @param height A numeric. Default is null to fit text grob. 
+  #' @param row_margin A numeric. Default is 1.5. This makes rows 1.5 heights of text grobs. 
   #' 
   #' @return Lauyout grob
   #' 
   #' @export
-tg2layout <- function(tg, 
-  width=NULL, 
-  height=NULL, 
-  row_margin=1.5  # height = height * row_margin
+tg2layout <- function(tg, width=NULL, height=NULL, row_margin=1.5  # height = height * row_margin
   ){
   nrow <- length(tg)
   grid::grid.layout(nrow, 1, width=grid::unit(841, "mm"), height=grid::unit(rep(1.5, nrow), "grobheight", tg))
   # layout <- grid::grid.layout(nrow, 1, width=unit(841, "mm"), height=unit(1, "null"))
 }
 
-  #' Title. 
+  #' Arrange text grob with layout.
   #' 
   #' @param tg A text grob
   #' @param layout A Grid layout, or NULL. 
-  #'        This can be used to initialise the frame with a number of 
-  #'        rows and columns, with initial widths and heights, etc.
+  #'        This can be used to initialise the frame with a number of rows.
   #' @param name A character identifier of frame name. 
   #' 
   #' @export
