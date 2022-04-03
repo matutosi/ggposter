0ca6aa10995c99dce0a5cf3f43951fe52a39a488
matutosi <matutosi@gmail.com>
2022-04-04 08:45:10 +0900

    add utils same that create same argument like "argument = argument, ..."

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,4 +1,18 @@
 test_that("dots2list works", {
-  expect_equal(dots2list(1,2,3,4), list(1,2,3,4))
-  expect_equal(dots2list(list(1,2,3,4)), list(1,2,3,4))
+  expect_equal(list(1,2,3,4), dots2list(1,2,3,4))
+  expect_equal(list(1,2,3,4), dots2list(list(1,2,3,4)))
+})
+
+test_that("as_same can create 'argument = argument, ...'", {
+  x <- "a"
+  times <- 10
+  expect_equal("x = x, times = times", as_same(x, times))
+  expect_equal(list(1,2,3,4), dots2list(list(1,2,3,4)))
+})
+
+test_that("same can create the same result", {
+  x <- "a"
+  times <- 10
+  expect_equal(rep(x = x, times = times), do.call(rep.int, same(x, times)))
+  expect_equal(rep(x = x, times = times), do.call(rep.int, same(times, x)))
 })
