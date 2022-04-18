fb8fc90740d4df38431b587cb80d0efe95a7ad80
matutosi <matutosi@gmail.com>
2022-04-18 16:00:15 +0900

    reset arguments

--- R/rect_grob.R
+++ R/rect_grob.R
@@ -7,11 +7,12 @@
 #'
 #' @export
 add_rect <- function(grob, 
-                     width = NULL, height= NULL, space = NULL, ...) {
-  if(is.null(space))  space <- grid::unit(2, "mm")
-  space <- grid::convertUnit(space, unitTo = "mm")
-  if(is.null(width))  width  <- grob_widths(grob,  convert_to = "mm") + space
-  if(is.null(height)) height <- grob_heights(grob, convert_to = "mm") + space
-  rect <- shoot(grid::rectGrob, width, height, ...)
-  grid::gTree(children = grid::gList(rect, grob))
+                     width = NULL, height= NULL, space = NULL, 
+                     ...) {
+    if(is.null(space))  space <- grid::unit(2, "mm")
+    space <- grid::convertUnit(space, unitTo = "mm")
+    if(is.null(width))  width  <- grob_widths(grob,  convert_to = "mm") + space
+    if(is.null(height)) height <- grob_heights(grob, convert_to = "mm") + space
+    rect <- grid::rectGrob(width = width, height = height, ...)
+    grid::gTree(children = grid::gList(rect, grob))
 }
