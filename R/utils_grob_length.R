a968b2f757ecd138a71915ca8c5ef413e7a83f18
matutosi <matutosi@gmail.com>
2022-04-19 09:24:55 +0900

    turn back

--- R/utils_grob_length.R
+++ R/utils_grob_length.R
@@ -1,11 +1,11 @@
 #' @rdname combine_grobs
 #' @export
 grob_widths <- function(grobs, convert_to=NULL){
-  if(length(grobs) == 1){
-    if(grid::convertUnit(grid::grobWidth(grobs), unitTo = "mm", valueOnly = TRUE) == 0){
-      if(is.grtree(grobs)) grobs <- grobs$children
-    }
-  }
+  #   if(length(grobs) == 1){
+  #     if(grid::convertUnit(grid::grobWidth(grobs), unitTo = "mm", valueOnly = TRUE) == 0){
+  #       if(is.grtree(grobs)) grobs <- grobs$children
+  #     }
+  #   }
   n <- if(grid::is.grob(grobs)) 1 else length(grobs)
   widths <- grid::unit(rep(1, n), rep("grobwidth", n), grobs)
   if(!is.null(convert_to)) widths <- grid::convertUnit(widths, convert_to)
@@ -15,11 +15,11 @@ grob_widths <- function(grobs, convert_to=NULL){
 #' @rdname combine_grobs
 #' @export
 grob_heights <- function(grobs, convert_to=NULL){
-  if(length(grobs) == 1){
-    if(grid::convertUnit(grid::grobHeight(grobs), unitTo = "mm", valueOnly = TRUE) == 0){
-      if(is.grtree(grobs)) grobs <- grobs$children
-    }
-  }
+  #   if(length(grobs) == 1){
+  #     if(grid::convertUnit(grid::grobHeight(grobs), unitTo = "mm", valueOnly = TRUE) == 0){
+  #       if(is.grtree(grobs)) grobs <- grobs$children
+  #     }
+  #   }
   n <- if(grid::is.grob(grobs)) 1 else length(grobs)
   heights <- grid::unit(rep(1, n), rep("grobheight", n), grobs)
   if(!is.null(convert_to)) heights <- grid::convertUnit(heights, convert_to)
